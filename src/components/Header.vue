cc1480885e94a5004823112ea39cab46f44064b4
Leo Qin <706046320@qq.com>
2018-03-11 12:26:28 +0800

    modify header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,19 +5,25 @@
   <button class="am-topbar-btn am-topbar-toggle am-btn am-btn-sm am-btn-success am-show-sm-only blog-button" data-am-collapse="{target: '#blog-collapse'}" ><span class="am-sr-only">导航切换</span> <span class="am-icon-bars"></span></button>
     <div class="am-collapse am-topbar-collapse" id="blog-collapse">
       <ul class="am-nav am-nav-pills am-topbar-nav">
-        <li class="am-active"><router-link to="/">Articles</router-link></li>
+        <li><router-link to="/">Articles</router-link></li>
         <li class="am-dropdown" data-am-dropdown>
-          <a class="am-dropdown-toggle" data-am-dropdown-toggle href="javascript:;">
-            首页布局 <span class="am-icon-caret-down"></span>
+          <a class="am-dropdown-toggle" data-am-dropdown-toggle href="javascript:void(0);">
+            Category <span class="am-icon-caret-down"></span>
           </a>
           <ul class="am-dropdown-content">
-            <li><a href="lw-index.html">1. blog-index-standard</a></li>
-            <li><a href="lw-index-nosidebar.html">2. blog-index-nosidebar</a></li>
-            <li><a href="lw-index-center.html">3. blog-index-layout</a></li>
-            <li><a href="lw-index-noslider.html">4. blog-index-noslider</a></li>
+
+            <li v-for="item in titleCate"><router-link to="">{{ item.name }}</router-link></li>
           </ul>
         </li>
-        <li><a href="lw-article.html">Ablum</a></li>
+        <li class="am-dropdown" data-am-dropdown>
+          <a class="am-dropdown-toggle" data-am-dropdown-toggle href="javascript:void(0);">
+            Ablum <span class="am-icon-caret-down"></span>
+          </a>
+          <ul class="am-dropdown-content">
+            <li><router-link to="">All</router-link></li>
+            <li v-for="item in imageCate"><router-link to="">{{ item.name }}</router-link></li>
+          </ul>
+        </li>
         <li><router-link to="">Timeline</router-link></li>
       </ul>
       <form class="am-topbar-form am-topbar-right am-form-inline" role="search">
@@ -32,7 +38,29 @@
 </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
+  data(){
+    return{
+      // 文章标题分类
+      titleCate:[],
+      // 图片分类
+      imageCate:[]
+    }
+  },
+  mounted(){
+    this.init();
+  },
+  methods:{
+    init(){
+      axios.get('/titlecate').then((response)=>{
+        this.titleCate = response.data;
+      });
+      axios.get('/imagecate').then((response)=>{
+        this.imageCate = response.data;
+      });
+    }
+  }
 
 }
 </script>
