de949cd92be96a844cb8ab38dc685a05086677cb
Leo Qin <706046320@qq.com>
2018-03-11 12:32:38 +0800

    modify aboutme and footer

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -4,10 +4,8 @@
     <div class="blog-sidebar-widget blog-bor">
         <h2 class="blog-text-center blog-title"><span>About ME</span></h2>
         <img src="static/i/me.jpeg" alt="about me" class="blog-entry-img" >
-        <p>妹纸</p>
-        <p>
-    我是妹子UI，中国首个开源 HTML5 跨屏前端框架
-    </p><p>我不想成为一个庸俗的人。十年百年后，当我们死去，质疑我们的人同样死去，后人看到的是裹足不前、原地打转的你，还是一直奔跑、走到远方的我？</p>
+        <p>秦梁(Leo)</p>
+        <p>写博客:一方面,为了找到更多志同道合的人;另一方面,为了更好地学习,扩大影响力.对于任何一个程序员来说是极其必要的.我的博客内容包括了,技术笔记,生活图片库.对我来说,博客记录我的成长过程,更可以让未来的我看到现在的不成熟想法与创意.</p>
     </div>
     <div class="blog-sidebar-widget blog-bor">
         <h2 class="blog-text-center blog-title"><span>Contact ME</span></h2>
