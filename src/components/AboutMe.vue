20d953169896c30ec97c637cfc1c34d1b36a8da0
Leo Qin <706046320@qq.com>
2018-03-12 21:50:48 +0800

    modify aboutme

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -7,25 +7,34 @@
         <p>秦梁(Leo)</p>
         <p>写博客:一方面,为了找到更多志同道合的人;另一方面,为了更好地学习,扩大影响力.对于任何一个程序员来说是极其必要的.我的博客内容包括了,技术笔记,生活图片库.对我来说,博客记录我的成长过程,更可以让未来的我看到现在的不成熟想法与创意.</p>
     </div>
-    <div class="blog-sidebar-widget blog-bor">
+    <div class="blog-sidebar-widget blog-bor logo-box">
         <h2 class="blog-text-center blog-title"><span>Contact ME</span></h2>
-        <p>
-            <a href=""><span class="am-icon-qq am-icon-fw am-primary blog-icon"></span></a>
-            <a href=""><span class="am-icon-github am-icon-fw blog-icon"></span></a>
-            <a href=""><span class="am-icon-weibo am-icon-fw blog-icon"></span></a>
-            <a href=""><span class="am-icon-reddit am-icon-fw blog-icon"></span></a>
-            <a href=""><span class="am-icon-weixin am-icon-fw blog-icon"></span></a>
-        </p>
+        <ul>
+          <li>
+              <a href="https://github.com/LeoQin0816" target="_blank"><span class="am-icon-github am-icon-fw blog-icon"></span></a>
+          </li>
+          <li>
+              <a href="https://gitlab.com/LeoQin0816" target="_blank"><span class="am-icon-gitlab am-icon-fw blog-icon"></span></a>
+          </li>
+          <li class="my-qq">
+              <a href="javascript:void(0);" target="_blank"><span class="am-icon-qq am-icon-fw blog-icon"></span></a>
+              <p class="qq-logo"></p>
+          </li>
+          <li class="my-wechat">
+              <a href="javascript:void(0);" target="_blank"><span class="am-icon-weixin am-icon-fw blog-icon"></span></a>
+              <p class="wechat-logo"></p>
+          </li>
+          <li class="my-weibo">
+              <a href="javascript:void(0);" target="_blank"><span class="am-icon-weibo am-icon-fw blog-icon"></span></a>
+              <p class="weibo-logo"></p>
+          </li>
+      </ul>
     </div>
+    <br><br>
     <div class="blog-clear-margin blog-sidebar-widget blog-bor am-g ">
         <h2 class="blog-title"><span>TAG cloud</span></h2>
         <div class="am-u-sm-12 blog-clear-padding">
-        <a href="" class="blog-tag">amaze</a>
-        <a href="" class="blog-tag">妹纸 UI</a>
-        <a href="" class="blog-tag">HTML5</a>
-        <a href="" class="blog-tag">这是标签</a>
-        <a href="" class="blog-tag">Impossible</a>
-        <a href="" class="blog-tag">开源前端框架</a>
+        <router-link v-for="item in tagCate" :key="item.id" to="" class="blog-tag">{{ item.name }}</router-link>
         </div>
     </div>
 </div>
@@ -33,8 +42,23 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data(){
+    return{
+      tagCate:[],
+    }
+  },
+  mounted(){
+    this.init();
+  },
+  methods:{
+    init(){
+      axios.get('/tagcate').then((response)=>{
+        this.tagCate = response.data;
+      });
+    }
+  }
 }
 </script>
 
