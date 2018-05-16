3b1a1942a696d756261a2fd3d0ec036f9537fa32
leo <706046320@qq.com>
2018-05-16 21:17:05 +0800

    配置路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/views/Home';
-import Article from '@/views/Article';
-import Timeline from '@/views/Timeline';
-import Media from '@/views/Media';
+// import Home from '@/views/Home';
+// import Article from '@/views/Article';
+// import Timeline from '@/views/Timeline';
+// import Media from '@/views/Media';
 
 Vue.use(Router);
 
@@ -13,22 +13,26 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home,
+      // component: Home,
+      component:resolve=>require(['@/views/Home'],resolve)
     },
     {
       path: '/article/:id',
       name: 'Article',
-      component: Article,
+      // component: Article,
+      component:resolve=>require(['@/views/Article'],resolve)
     },
     {
       path: '/timeline',
       name: 'Timeline',
-      component: Timeline,
+      // component: Timeline,
+      component:resolve=>require(['@/views/Timeline'],resolve)
     },
     {
       path: '/media',
       name: 'Media',
-      component: Media,
+      // component: Media,
+      component:resolve=>require(['@/views/Media'],resolve)
     },
   ]
 });
